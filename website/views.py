6c66ea2b4297a43f1cf518d8a9aa56bd93e8f45b
Mohamadreza Rm <mmdrzaaa.1997@gmail.com>
2026-06-10 20:42:34 +0430

    added general templates folder
    moved website templates to general folder of templates

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
 
 def home_view(request):
     return render(request, 'index.html')
