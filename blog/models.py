b74a4f3aaef18df6f5072a7e3eea5491b397b40e
Mohamadreza Rm <mmdrzaaa.1997@gmail.com>
2026-07-05 14:41:31 +0330

    fix: correct max_length
    fix: correct max_length in models.py and 0001_initial.py

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,5 @@ from django.db import models
 # Create your models here.
 
 class post(models.Model):
-    title = models.CharField(length= 250)
     title = models.CharField(max_length = 255)
     content = models.TextField()
